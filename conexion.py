3e87cbb7f03702d2c33c185080af5dbb9202652b
Wilson365 <carlos.wilson@uabc.edu.mx>
2025-02-27 22:15:44 -0800

    Update conexion.py

--- conexion.py
+++ conexion.py
@@ -1,5 +1,12 @@
 import cx_Oracle
-
+""""
+Al conectar con una base de datos Oracle, se establece una sesión con el servidor
+a través del puerto configurado,
+lo que permite el flujo de información entre el cliente y la base de datos. Python 
+facilita esta conexión mediante 
+el módulo cx_Oracle, que actúa como un driver para interactuar con Oracle Database
+utilizando su API nativa
+""""
 dsn = cx_Oracle.makedsn("localhost", 1521, service_name="XEPDB1")
 conn = cx_Oracle.connect(user="usuario", password="contraseña", dsn=dsn)
 
